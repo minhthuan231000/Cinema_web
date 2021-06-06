8549bd9c50167c25d1e3fe16f6532edc5fda1103
minhthuan231000 <70680871+minhthuan231000@users.noreply.github.com>
2021-06-06 22:56:18 +0700

    Add files via upload

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,30 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import logo from '../../images/Logo.jpg'
 class HeaderTop extends Component {
     render() {
         return (
-            <div className="HeaderTop">
-                <Container>
-                    <Row>
-                        <Col xs="3">
-                            <img className="img-logo" src={logo} alt="IMG" />
-                        </Col>
-                        <Col className="navbox" xs="9">
-                            <Row className="navrow">
-                                <Col id="phim" className="navitem" >
-                                    <a href="/Phim">PHIM</a>
-                                </Col>
-                                <Col id="lichchieu" className="navitem" >
-                                    <a href="/Lichchieu">LỊCH CHIẾU</a>
-                                </Col>
-                                <Col id="rapvagia" className="navitem" >
-                                    <a href="/RapvaGia">RẠP VÀ GIÁ</a>
-                                </Col>
-                                <Col id="tintuc" className="navitem" >
-                                    <a href="/Tintuc">TIN TỨC</a>
-                                </Col>
-                                <Col id="gioithieu" className="navitem" >
-                                    <a href="/Gioithieu">GIỚI THIỆU</a>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Container>
+            <div className="container HeaderTop">
+                <div><a className="logo" href="/Home">
+                </a>
+                </div>
+                <div className="navbox">
+                    <ul>
+                        <li id="phim">
+                            <a className="item" href="/Phim">PHIM</a>
+                        </li>
+                        <li id="lichchieu">
+                            <a className="item" href="/Lichchieu">LỊCH CHIẾU</a>
+                        </li>
+                        <li id="rapvagia">
+                            <a className="item" href="/RapvaGia">RẠP VÀ GIÁ</a>
+                        </li>
+                        <li id="tintuc">
+                            <a className="item" href="/Tintuc">TIN TỨC</a>
+                        </li>
+                        <li id="gioithieu">
+                            <a className="item" href="/Gioithieu">GIỚI THIỆU</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }
--- src/components/Member/Member.js
+++ src/components/Member/Member.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react';
-export default class Member extends Component {
-    render() {
-        return (
-            <div className="register-content">
-                <div className="container">
+import React, { Component, useState } from 'react';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+import './member.css'
+import Register from '../RegisterForm/Register';
+import Login from '../LoginForm/Login';
+export default function Member() {
+    const [openReg, setOpen] = useState(false);
+    const onOpenModalReg = () => setOpen(true);
+    const onCloseModalReg = () => setOpen(false);
+
+    const [openLog, setOpen2] = useState(false);
+    const onOpenModalLog = () => setOpen2(true);
+    const onCloseModalLog = () => setOpen2(false);
+    return (
+        <div className="register-content">
+            <div className="container">
                 <div className="register-wrap">
                     <ul>
                         <li className="btn-register">
-                            <a href="Dangky">Đăng ký</a>
+                            <button onClick={onOpenModalReg}>Đăng ký thành viên</button>
+                            <Modal open={openReg} onClose={onCloseModalReg} center classNames={{ overlay: 'customOverlay', modal: 'ModalReg' }}>
+                                <Register />
+                            </Modal>
                         </li>
                         <li className="btn-login">
-                            <a href="/login">Đăng nhập</a>
+                            <button onClick={onOpenModalLog}>Đăng nhập</button>
+                            <Modal open={openLog} onClose={onCloseModalLog} center classNames={{ overlay: 'customOverlay', modal: 'ModalLog' }}>
+                                <Login />
+                            </Modal>
                         </li>
-                        <a className="hotline" href="tel.08081508">0808 1508</a>
+                        <li className="hotline">0808 1508</li>
                     </ul>
                 </div>
-                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
