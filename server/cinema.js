ec9c14bfcab6d6f4d972524a19314a00cec61ed0
ntn1102 <82469794+ntn1102@users.noreply.github.com>
2021-06-23 17:41:42 +0700

    Add files via upload

---
+++ server/cinema.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const db = require('./db')
+const Utils = require("./utils");
+
+const Cinema = db.define("Cinema", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: Sequelize.TEXT,
+    allowNull: true
+  }
+});
+
+Cinema.associate = function (models) {
+  Cinema.hasMany(models.Theater, {
+    foreignKey: 'cinema_id',
+    sourceKey: 'id',
+    as: 'theaters'
+  });
+};
+
+module.exports = Cinema;
