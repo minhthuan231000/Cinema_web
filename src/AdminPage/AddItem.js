68c502ad32b8e0599c959a3535fe35f49b2d1f3a
minhthuan231000 <minhthuan23100@gmail.com>
2021-07-05 00:58:03 +0700

    update AdPage

--- src/AdminPage/AddItem.js
+++ src/AdminPage/AddItem.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
@@ -12,44 +11,18 @@ import RemoveIcon from '@material-ui/icons/Remove';
 import Fab from '@material-ui/core/Fab';
 import QueueIcon from '@material-ui/icons/Queue';
 import ImageUpload from "./ImageUpload";
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'block',
-    flexWrap: 'wrap',
-    color: 'black'
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  addButton: {
-    height: 10,
-    width: 30,
-  },
-  reduceButton: {
-    height: 10,
-    width: 30,
-  },
-  size: {
-    margin: 8,
-    width: '1em',
-  },
-  resizeGroup: {
-    display: 'inline',
-  }
-}));
 
+import './admin.css'
 
 export default function FeatureAdd() {
-  const classes = useStyles();
 
   const [type, setType] = React.useState('');
   const handleChangeType = (event) => {
     setType(event.target.value);
   }
 
-  const [countX, setCountX] = React.useState(1);
-  const [countY, setCountY] = React.useState(1);
+  const [countX, setCountX] = React.useState(10);
+  const [countY, setCountY] = React.useState(10);
   const handleSubmit1 = () => {
 
   }
@@ -57,86 +30,99 @@ export default function FeatureAdd() {
 
   }
   return (
-    <div>
-      <form className={classes.root} autoComplete="auto" >
-        <div>
-          <label><h4>Thêm rạp</h4></label>
-        </div>
-        <TextField style={{ margin: 8 }} id="standard-basic" label="Tên Rạp" />
-        <FormControl className={classes.formControl}>
-          <InputLabel id="demo-simple-select-label">Loại Rạp</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={type}
-            onChange={handleChangeType}
-          >
-            <MenuItem value={'2D'}>2D</MenuItem>
-            <MenuItem value={'3D'}>3D</MenuItem>
-            <MenuItem value={'4DX'}>4DX</MenuItem>
-          </Select>
-        </FormControl>
-        <div>
-          <label><h6>Cấu hình ghế</h6></label>
-        </div>
-        <InputLabel id="demo-simple-select-label">Chiều ngang</InputLabel>
-        <Fab size="small" color="primary" aria-label="chieungang" onClick={() => {
-          setCountX(Math.max(countX - 1, 10));
-        }} className={classes.addButton}>
-          <RemoveIcon fontSize="small" />
-        </Fab>
-        <TextField className={classes.size} value={countX} />
-        <Fab size="small" color="primary" aria-label="chieungang" onClick={() => {
-          setCountX(Math.min(countX + 1, 20));
-        }} className={classes.reduceButton} >
-          <AddIcon fontSize="small" />
-        </Fab>
-        <InputLabel id="demo-simple-select-label">Chiều dọc</InputLabel>
-        <Fab size="small" color="primary" aria-label="chieudoc" onClick={() => {
-          setCountY(Math.max(countY - 1, 10));
-        }} className={classes.addButton}>
-          <RemoveIcon fontSize="small" />
-        </Fab>
-        <TextField className={classes.size} value={countY} />
-        <Fab size="small" color="primary" aria-label="chieudoc" onClick={() => {
-          setCountY(Math.min(countY + 1, 20));
-        }} className={classes.reduceButton} >
-          <AddIcon fontSize="small" />
-        </Fab>
-        <hr />
-        <div>
-          <label><h4>Thêm cụm rạp</h4></label>
-        </div>
-        <TextField style={{ margin: 8 }} id="standard-basic" label="Tên Cụm Rạp" />
-        <TextField style={{ margin: 8 }} id="standard-basic" label="Địa Chỉ" />
-        <hr />
-        <div>
-          <label><h4>Thêm Suất Chiếu</h4></label>
-        </div>
-        <TextField style={{ margin: 8 }} id="standard-basic" label="Thời điểm bắt đầu" />
-        <TextField style={{ margin: 8 }} id="standard-basic" label="Thời điểm kết thúc" />
-        <p></p>
-        <InputLabel id="demo-simple-select-label">Submit</InputLabel>
-        <Fab size="small" color="secondary" aria-label="submit" className={classes.reduceButton} onSubmit={handleSubmit1} >
-          <QueueIcon />
-        </Fab>
-      </form>
-      <form className={classes.root} autoComplete="auto">
-        <hr />
-        <div>
-          <label><h4>Thêm Phim</h4></label>
-        </div>
-        <TextField style={{ margin: 8 }} id="standard-basic" label="Tên Phim" />
-        <TextField style={{ margin: 8 }} id="standard-basic" label="Thời lượng (phút)" />
-        <TextField style={{ margin: 8 }} id="standard-basic" label="Ngày công chiếu" />
+    <div className="addFeature-root">
+      <div className="col-5" >
+        <form autoComplete="auto" >
+          <div className="groupAdd-rap">
+            <label><h4>Thêm rạp</h4></label>
+            <div className="groupControl-rap">
+              <TextField style={{ marginInlineEnd: '0.5em' }} id="textField-TenRap" label="Tên Rạp" />
+              <FormControl className="input-TypeRap">
+                <InputLabel id="demo-simple-select-label">Loại Rạp</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={type}
+                  onChange={handleChangeType}
+                >
+                  <MenuItem value={'2D'}>2D</MenuItem>
+                  <MenuItem value={'3D'}>3D</MenuItem>
+                  <MenuItem value={'4DX'}>4DX</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className="groupCauHinhGhe">
+              <div><label><h6>Cấu hình ghế</h6></label></div>
+              <div className="groupSize">
+                <InputLabel>Chiều ngang</InputLabel>
+                <Fab size="small" color="primary" aria-label="chieungang" onClick={() => {
+                  setCountX(Math.max(countX - 1, 10));
+                }} className="">
+                  <RemoveIcon fontSize="small" />
+                </Fab>
+                <TextField helperText="Max 20" className="textField-SizeRap" value={countX} />
+                <Fab size="small" color="primary" aria-label="chieungang" onClick={() => {
+                  setCountX(Math.min(countX + 1, 20));
+                }} className="">
+                  <AddIcon fontSize="small" />
+                </Fab>
+              </div>
+              <div className="groupSize">
+                <InputLabel>Chiều dọc</InputLabel>
+                <Fab size="small" color="primary" aria-label="chieudoc" onClick={() => {
+                  setCountY(Math.max(countY - 1, 10));
+                }} className="">
+                  <RemoveIcon fontSize="small" />
+                </Fab>
+                <TextField helperText="Max 20" className="textField-SizeRap" value={countY} />
+                <Fab size="small" color="primary" aria-label="chieudoc" onClick={() => {
+                  setCountY(Math.min(countY + 1, 20));
+                }} className="">
+                  <AddIcon fontSize="small" />
+                </Fab>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="groupCumRap">
+            <div><label><h4>Thêm cụm rạp</h4></label></div>
+            <TextField id="standard-basic" label="Tên Cụm Rạp" />
+            <TextField style={{ marginTop: 8 }} id="standard-basic" fullWidth={true} label="Địa Chỉ" />
+          </div>
+          <hr />
+          <div className="groupSuatChieu">
+            <div>
+              <label><h4>Thêm Suất Chiếu</h4></label>
+            </div>
+            <TextField style={{ marginTop: 3, marginRight: 8 }} id="standard-basic" label="Thời điểm bắt đầu" />
+            <TextField style={{ marginTop: 3 }} id="standard-basic" label="Thời điểm kết thúc" />
+            <p></p>
+            <InputLabel id="demo-simple-select-label">Submit</InputLabel>
+            <Fab size="small" color="secondary" aria-label="submit" className="" onSubmit={handleSubmit1} >
+              <QueueIcon />
+            </Fab>
+          </div>
+        </form>
+      </div>
+      <div className="col-5" >
+        <form autoComplete="auto">
+          <div className="groupAdd-phim">
+            <div>
+              <label><h4>Thêm Phim</h4></label>
+            </div>
+            <TextField style={{ margin: 8 }} id="standard-basic" label="Tên Phim" />
+            <TextField style={{ margin: 8 }} id="standard-basic" label="Thời lượng (phút)" />
+            <TextField style={{ margin: 8 }} id="standard-basic" label="Ngày công chiếu" placeholder="(MM/DD/YYYY)" />
 
-        <ImageUpload cardName="Input Image" />
-        <p></p>
-        <InputLabel id="demo-simple-select-label">Submit</InputLabel>
-        <Fab size="small" color="secondary" aria-label="submit" className={classes.reduceButton} onSubmit={handleSubmit2} >
-          <QueueIcon />
-        </Fab>
-      </form>
+            <ImageUpload cardName="Input Image" />
+            <p></p>
+            <InputLabel id="demo-simple-select-label">Submit</InputLabel>
+            <Fab size="small" color="secondary" aria-label="submit" className="" onSubmit={handleSubmit2} >
+              <QueueIcon />
+            </Fab>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
