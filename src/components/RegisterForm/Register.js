2aed64f282776e41aa8de8ed01072f8b93856420
minhthuan231000 <minhthuan23100@gmail.com>
2021-07-09 00:16:03 +0700

    Update Register.js

--- src/components/RegisterForm/Register.js
+++ src/components/RegisterForm/Register.js
@@ -1,40 +1,77 @@
 import React, { Component } from 'react';
-import './Register.css'
+import './Register.css';
 import { isEmail, isEmpty, isLength, isContainWhiteSpace } from '.../../../models/validator';
-const post_server = "9080";
+const post_server = process.env.POST_SERVER || "9080";
 class Register extends Component {
   /* Xử lý nodejs tại component này */
   constructor() {
     super();
     this.state = {
-      gender: null,
-      user: [],
-      formData: {},
+      email: '',
       errors: null, // Contains login field errors
-      isRegisted: false
+      isRegister: false
     }
   }
 
   render() {
     const ShowConfirmEmail = () => {
-      const formData = {};
+      let token = {};
+
       const handleInputChange = (event) => {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-        formData[name] = value;
+        token[name] = value;
       }
+      const onSubmit = ((e) => {
+        //Chặn các event mặc định của form
+        e.preventDefault();
+        if (token == null) {
+          // vui lòg nhập đầy đủ thôg tin để dki
+          alert('Please fill in the information to complete the registration')
+        } else {
+          let data = {
+            ...token,
+            email: this.state.email
+          };
+          //Login code
+          let request = new Request(`http://localhost:${post_server}/api/confirm`, {
+            method: 'POST',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify(data)
+          });
+
+          fetch(request)
+            .then(res => res.json())
+            .then((result) => {
+              console.log(result.Status);
+              if (result.Status === 'Complete') {
+                alert("Đăng kí thành công!");
+                window.location.reload();
+              } else if (result.Status === 'Invalid') {
+                alert("Thử lại!");
+              }
+            },
+              (error) => {
+                this.setState({
+                  error: error
+                });
+                if (error) {
+                  console.log(error);
+                }
+              }
+            )
+        }
+      })
       return (
         <div>
-          <form method="POST">
-            <h6>Nhập mã xác thực (Vui lòng kiểm tra email)</h6>
-            <div className="form-group">
-              <input type="email" name='email' className="form-control" onChange={handleInputChange} placeholder="EMAIL (*)" />
-            </div>
-            <div className="forget-password">
-              <button type="submit" className="btn btn-dark btn-lg btn-block col-5 btnForget">Hoàn Tất Đăng Ký</button>
-            </div>
-          </form>
+          <h6>Nhập mã xác thực (Vui lòng kiểm tra email)</h6>
+          <div className="form-group">
+            <input type="text" name='token' className="form-control" onChange={handleInputChange} placeholder="CODE (*)" />
+          </div>
+          <div className="forget-password">
+            <button onClick={e => onSubmit(e)} type="button" className="btn btn-dark btn-lg btn-block col-5 btnForget">Hoàn Tất Đăng Ký</button>
+          </div>
         </div>
       )
     }
@@ -46,13 +83,11 @@ class Register extends Component {
         const value = target.value;;
         const name = target.name;
         formData[name] = value;
-        this.setState({
-          formData: formData,
-        }) 
       }
+
+      // Note: this will *not* work as intended.
       const validateLoginForm = (e) => {
         let errors = {};
-        const { formData } = this.state;
         if (isEmpty(formData.email)) {
           errors.email = "Email can't be blank";
         } else if (!isEmail(formData.email)) {
@@ -94,65 +129,70 @@ class Register extends Component {
         e.preventDefault();
         //Gọi hàm validateLoginForm() dùng để kiểm tra form
         const validation = validateLoginForm();
+
         //Kiểm tra lỗi của input trong form và hiển thị
         if (validation !== true) {
-          alert(Object.values(validation))
-        } else {
-          //Login code
-          const { formData } = this.state;
-          let data = {
-            fullname: formData.fullname,
-            phone: formData.phone,
-            gender: formData.gender,
-            email: formData.email,
-            password: formData.password,
-            role: "user"
-
+          // vui lòg nhập đầy đủ thôg tin để dki
+          if (validation.confirm) {
+            alert(validation.confirm);
+          } else {
+            alert('Please fill in the information to complete the registration')
           };
-          if (formData) {
-            var request = new Request(`http://localhost:${post_server}/api/register`, {
-              method: 'POST',
-              headers: new Headers({ 'Content-Type': 'application/json' }),
-              body: JSON.stringify(data)
-            });
+        } else {
+          let data = {
+            ...formData,
+            role: "user"
+          };
+          //Login code
+          let request = new Request(`http://localhost:${post_server}/api/register`, {
+            method: 'POST',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify(data)
+          });
 
-            fetch(request)
-              .then(res => res.json())
-              .then((result) => {
-                console.log(result)
-                this.setState({
-                  isRegisted: true // Nếu đăng ký thành công thì gửi mã xác thực, chỗ này code mã xác thực
-                })
-                refreshPage();
-              },
-                (error) => {
-                  this.setState({
-                    error: error
-                  });
-                  if (error) {
-                    console.log(error);
-                  }
+          fetch(request)
+            .then(res => res.json())
+            .then((result) => {
+              if (result) {
+                if (result.Status === 'Complete') {
+                  this.setState({ isRegister: true });
+                  this.setState({ email: data.email })
+                } else if (result.Status === 'Email is exist') {
+                  alert(result.Status)
                 }
-              )
-          }
+              }
+            },
+              (error) => {
+                this.setState({
+                  error: error
+                });
+                if (error) {
+                  console.log(error);
+                }
+              }
+            )
         }
       }
 
-      const refreshPage = () => {
-        window.location.reload();
-      }
+      // const refreshPage = () => {
+      //   window.location.reload();
+      // }
       return <div className="Register container" >
         <form method="POST">
           <center><h3>Đăng ký</h3></center>
           <div className="form-group col-6 Name">
-            <input type="text" name="fullname" className="form-control" onChange={e => handleInputChange(e)} placeholder="HỌ TÊN (*)" />
+            <input type="text" name="fullname" className="form-control" onChange={handleInputChange} placeholder="HỌ TÊN (*)" />
           </div>
           <div className="form-group col-6 Tel">
-            <input type="text" name="phone" className="form-control" onChange={e => handleInputChange(e)} placeholder="ĐIỆN THOẠI (*)" />
+            <input type="text" name="phone" className="form-control" onChange={handleInputChange} placeholder="ĐIỆN THOẠI (*)" />
           </div>
-          <div className="form-group" onChange={e => handleInputChange(e)}>
+          <div className="form-group" onChange={handleInputChange}>
             <div className="form-check">
+<<<<<<< HEAD
               <input className="form-check-input" type="radio" name="gender" value="Nam"/>
+=======
+              <input className="form-check-input" type="radio" name="gender" value="Nam" />
+>>>>>>> 3c6af53bf860ce1d40a0f4770b759a50f6336975
               <label className="form-check-label" htmlFor="flexRadioDefault1">NAM</label>
             </div>
             <div className="form-check">
@@ -161,20 +201,20 @@ class Register extends Component {
             </div>
           </div>
           <div className="form-group">
-            <input type="email" name="email" className="form-control" onChange={e => handleInputChange(e)} placeholder="EMAIL (*)" />
+            <input type="email" name="email" className="form-control" onChange={handleInputChange} placeholder="EMAIL (*)" />
           </div>
           <div className="form-group">
-            <input type="password" name="password" className="form-control" onChange={e => handleInputChange(e)} placeholder="MẬT KHẨU (*)" />
+            <input type="password" name="password" className="form-control" onChange={handleInputChange} placeholder="MẬT KHẨU (*)" />
           </div>
           <div className="form-group">
-            <input type="password" name="confirm_password" className="form-control" onChange={e => handleInputChange(e)} placeholder="NHẬP LẠI MẬT KHẨU (*)" />
+            <input type="password" name="confirm_password" className="form-control" onChange={handleInputChange} placeholder="NHẬP LẠI MẬT KHẨU (*)" />
           </div>
-          <button onClick={submitForm} id="btnSubmitForm" type="submit" className="btn btn-dark btn-lg btn-block col-5 btnReg">Đăng ký</button>
+          <button onClick={e => submitForm(e)} id="btnSubmitForm" type="submit" className="btn btn-dark btn-lg btn-block col-5 btnReg">Đăng ký</button>
         </form>
       </div>
     }
     const handleShow = () => {
-      return this.state.isRegisted ? <ShowConfirmEmail /> : <ShowReg />
+      return this.state.isRegister ? <ShowConfirmEmail /> : <ShowReg />
     }
     return (
       <div>
