a7473132cdb3e0adb90376ab1465b3a4d1a34cc3
jinjutroy <75969059+jinjutroy@users.noreply.github.com>
2021-07-10 22:00:13 +0700

    load-movie-desc

--- server/routers/load/movies.js
+++ server/routers/load/movies.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 const Movie = require('../../models').Movie;
 router.post('/',asyncHandler(async function(request, response){
-    const found = await Movie.findAll();
+    const found = await Movie.findAll({
+      order: [
+        ['view', 'DESC'],
+    ],
+    });
     if(found){
         return response.status(200).send( { Status: 'Complete',
                                             movie: found});
