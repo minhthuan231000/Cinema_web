76958ebe6d626d9720e3d99dd00c1cc21956e6e3
minhthuan231000 <minhthuan23100@gmail.com>
2021-07-14 00:01:34 +0700

    Update InfoForm.js

--- src/components/InfoForm/InfoForm.js
+++ src/components/InfoForm/InfoForm.js
@@ -7,18 +7,18 @@ export default class Info extends Component {
       <div className="Info">
         <h2>Thông tin tài khoản</h2>
         <div className="form-group">
-          <label>EMAIL:</label> 
+          <label>EMAIL:</label>
           <input type="text" className="form-control" name='emai' value={user.email} readOnly={true} />
         </div>
         <div className="form-group">
           <label>HỌ TÊN:</label>
-          <input type="text" className="form-control" name='fullname' value={user.fullname} readOnly={true}/>
+          <input type="text" className="form-control" name='fullname' value={user.fullname} readOnly={true} />
         </div>
         <div className="form-group">
           <label>SỐ ĐIỆN THOẠI:</label>
           <input type="text" className="form-control" name='phone' value={user.numphone} readOnly={true} />
         </div>
-        <label><i><small><a href="/History" style={{color: 'white'}}>LICH SỬ ĐẶT VÉ</a></small></i></label>
+        <label><i><small><a href="/History" style={{ color: 'white' }}>LICH SỬ ĐẶT VÉ</a></small></i></label>
       </div>
     );
   }
