4ebc4e77b554886bfe43a3ffe8d3070cb97a98fc
minhthuan231000 <minhthuan23100@gmail.com>
2021-07-18 16:16:55 +0700

    Update Revenue.js

--- src/AdminPage/Revenue.js
+++ src/AdminPage/Revenue.js
@@ -12,46 +12,70 @@ import TextField from '@material-ui/core/TextField';
 import moment from 'moment';
 import './admin.css'
 
+const DOMAIN = process.env.REACT_APP_DOMAIN;
 const TAX_RATE = 0.1;
-
-/* function format_curency(a) {
-    a.value = a.value.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-} */
-
-function priceRow(qty, unit) {
-    return qty * unit;
+const loggedInUser = JSON.parse(sessionStorage.getItem('user'));
+const theater = JSON.parse(localStorage.getItem('theater') || '0');
+const booking = JSON.parse(localStorage.getItem('booking') || '0');
+const movie = JSON.parse(localStorage.getItem('movie') || '0');
+console.log(booking)
+const rows = createListRow(booking);
+console.log(rows)
+function search(nameKey, myArray) {
+    for (var i = 0; i < myArray.length; i++) {
+        if (myArray[i].id === nameKey) {
+            return myArray[i];
+        }
+    }
 }
 
-function createRow(name, qty, unit) {
-    const price = priceRow(qty, unit);
-    return { name, qty, unit, price };
+//Name Movie
+function getNameMovie(id) {
+    return search(id, movie).name;
 }
-
+function getNameTheater(id) {
+    return search(id, theater).name;
+}
+//Get chair_id
+function getListChair(list_tickets) {
+    let count = 0;
+    let total = 0;
+    let chair = '';
+    let price = 0;
+    count = list_tickets.length;
+    list_tickets.map((p) => (price = p.price));
+    list_tickets.map((ticket) => (total += ticket.price));
+    list_tickets.map((str) => (
+        chair += str.chair_id + ' '
+    ));
+    return [count, chair, price, total];
+}
+function createRow(bookings) {
+    let name = getNameMovie(bookings.showtime.movie_id);
+    let seat = getListChair(bookings.tickets)[1];
+    let quantity = getListChair(bookings.tickets)[0];
+    let unit = "Ticket";
+    let total_price = getListChair(bookings.tickets)[3];;
+    return [name, seat, quantity, unit, total_price];
+}
+function createRow2(bookings) {
+    let name = getNameTheater(bookings.showtime.thea_id);
+    let seat = getListChair(bookings.tickets)[1];
+    let quantity = getListChair(bookings.tickets)[0];
+    let unit = "Ticket";
+    let total_price = getListChair(bookings.tickets)[3];;
+    return [name, seat, quantity, unit, total_price];
+}
+function createListRow(booking_obj) {
+    let rows = [];
+    if (booking_obj) {
+        booking_obj.map((b) => (rows.push(createRow(b))));
+    }
+    return rows;
+}
 function subtotal(items) {
-    return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
+    return items.map((total_price) => total_price).reduce((sum, i) => sum + i, 0);
 }
-
-const rows = [
-    createRow('Cụm 1', 100, 45000),
-    createRow('Cụm 2', 10, 65000),
-    createRow('Cụm 3', 2, 55000),
-];
-
-const rows2 = [
-    createRow('Avenger:	Endgame', 100, 45000),
-    createRow('Spiderman 3', 120, 65000),
-    createRow('Flash 3', 50, 55000),
-];
-
-// Tính toán thống kê theo cụm rạp
-const invoiceSubtotal = subtotal(rows);
-const invoiceTaxes = TAX_RATE * invoiceSubtotal;
-const invoiceTotal = invoiceSubtotal - invoiceTaxes;
-
-// Tính toán thống kê theo phim
-const invoiceSubtotal2 = subtotal(rows2);
-const invoiceTaxes2 = TAX_RATE * invoiceSubtotal2;
-const invoiceTotal2 = invoiceSubtotal2 - invoiceTaxes2;
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -75,149 +99,158 @@ export default function Revenue() {
     const handleChangeDate = e => {
         setDate(e.target.value);
     };
-    console.log(date)
-    return (
-        <div className="revenue_main">
-            <div className="revenue_content">
-                <h2>Thống kê doanh thu theo cụm rạp</h2>
-            </div>
-            <TableContainer component={Paper}>
-                <form className={classes.container} noValidate method="POST" >
-                    <TextField
-                        id="date"
-                        label="From"
-                        type="date"
-                        defaultValue={date}
-                        onChange={handleChangeDate}
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        id="date"
-                        label="To"
-                        type="date"
-                        defaultValue={date} 
-                        onChange={handleChangeDate}
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <button type="submit" className="btn btn-info">Load</button>
-                </form>
-                <Table aria-label="spanning table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center" colSpan={3}>
-                                Details
-                            </TableCell>
-                            <TableCell align="right">Price</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Tên Cụm Rạp</TableCell>
-                            <TableCell align="right">Số Lượng</TableCell>
-                            <TableCell align="right">Giá Vé</TableCell>
-                            <TableCell align="right">Doanh Thu</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow key={row.name}>
-                                <TableCell>{row.name}</TableCell>
-                                <TableCell align="right">{row.qty}</TableCell>
-                                <TableCell align="right">{row.unit}</TableCell>
-                                <TableCell align="right">{row.price}</TableCell>
+    if (!loggedInUser) {
+        window.location.href = "/";
+        return;
+    }
+    if (loggedInUser) {
+
+        const invoiceSubtotal = subtotal(rows.map(row => row[4]));
+        const invoiceTaxes = TAX_RATE * invoiceSubtotal;
+        const invoiceTotal = invoiceSubtotal - invoiceTaxes;
+        return (
+            <div className="revenue_main">
+                <div className="revenue_content">
+                    <h2>Thống kê doanh thu theo cụm rạp</h2>
+                </div>
+                <TableContainer component={Paper}>
+                    <form className={classes.container} noValidate method="POST" >
+                        <TextField
+                            id="date"
+                            label="From"
+                            type="date"
+                            defaultValue={date}
+                            onChange={handleChangeDate}
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <TextField
+                            id="date"
+                            label="To"
+                            type="date"
+                            defaultValue={date}
+                            onChange={handleChangeDate}
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <button type="submit" className="btn btn-info">Load</button>
+                    </form>
+                    <Table aria-label="spanning table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center" colSpan={3}>
+                                    Details
+                                </TableCell>
+                                <TableCell align="right">Price</TableCell>
                             </TableRow>
-                        ))}
-                        <TableRow>
-                            <TableCell rowSpan={3} />
-                            <TableCell colSpan={2}>Tổng phụ</TableCell>
-                            <TableCell align="right">{invoiceSubtotal}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Thuế</TableCell>
-                            <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
-                            <TableCell align="right">{invoiceTaxes}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell colSpan={2}>Tổng</TableCell>
-                            <TableCell align="right">{invoiceTotal}</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <div className="revenue_content">
-                <h2>Thống kê doanh thu theo phim</h2>
-            </div>
-            <TableContainer component={Paper}>
-                <form className={classes.container} noValidate method="POST" >
-                    <TextField
-                        id="date"
-                        label="From"
-                        type="date"
-                        defaultValue={date}
-                        onChange={handleChangeDate}
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        id="date"
-                        label="To"
-                        type="date"
-                        defaultValue={date}
-                        onChange={handleChangeDate}
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <button type="submit" className="btn btn-info">Load</button>
-                </form>
-                <Table aria-label="spanning table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center" colSpan={3}>
-                                Details
-                            </TableCell>
-                            <TableCell align="right">Price</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Tên Phim</TableCell>
-                            <TableCell align="right">Số Lượng</TableCell>
-                            <TableCell align="right">Giá Vé</TableCell>
-                            <TableCell align="right">Doanh Thu</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows2.map((row) => (
-                            <TableRow key={row.name}>
-                                <TableCell>{row.name}</TableCell>
-                                <TableCell align="right">{row.qty}</TableCell>
-                                <TableCell align="right">{row.unit}</TableCell>
-                                <TableCell align="right">{row.price}</TableCell>
+                            <TableRow>
+                                <TableCell>Tên Cụm Rạp</TableCell>
+                                <TableCell align="right">Số Lượng</TableCell>
+                                <TableCell align="right">Giá Vé</TableCell>
+                                <TableCell align="right">Doanh Thu</TableCell>
                             </TableRow>
-                        ))}
-                        <TableRow>
-                            <TableCell rowSpan={3} />
-                            <TableCell colSpan={2}>Tổng phụ</TableCell>
-                            <TableCell align="right">{invoiceSubtotal2}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>Thuế</TableCell>
-                            <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
-                            <TableCell align="right">{invoiceTaxes2}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell colSpan={2}>Tổng</TableCell>
-                            <TableCell align="right">{invoiceTotal2}</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-    );
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow key={row[0]}>
+                                    <TableCell>{row[0]}</TableCell>
+                                    <TableCell align="right">{row[2]}</TableCell>
+                                    <TableCell align="right">{row[4]}</TableCell>
+                                    <TableCell align="right">{row[4]}</TableCell>
+                                </TableRow>
+                            ))}
+                            <TableRow>
+                                <TableCell rowSpan={3} />
+                                <TableCell colSpan={2}>Tổng phụ</TableCell>
+                                <TableCell align="right">{invoiceSubtotal}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Thuế</TableCell>
+                                <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
+                                <TableCell align="right">{invoiceTaxes}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell colSpan={2}>Tổng</TableCell>
+                                <TableCell align="right">{invoiceTotal}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <div className="revenue_content">
+                    <h2>Thống kê doanh thu theo phim</h2>
+                </div>
+                <TableContainer component={Paper}>
+                    <form className={classes.container} noValidate method="POST" >
+                        <TextField
+                            id="date"
+                            label="From"
+                            type="date"
+                            defaultValue={date}
+                            onChange={handleChangeDate}
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <TextField
+                            id="date"
+                            label="To"
+                            type="date"
+                            defaultValue={date}
+                            onChange={handleChangeDate}
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <button type="submit" className="btn btn-info">Load</button>
+                    </form>
+                    <Table aria-label="spanning table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center" colSpan={3}>
+                                    Details
+                                </TableCell>
+                                <TableCell align="right">Price</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Tên Phim</TableCell>
+                                <TableCell align="right">Số Lượng</TableCell>
+                                <TableCell align="right">Giá Vé</TableCell>
+                                <TableCell align="right">Doanh Thu</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow key={row[0]}>
+                                    <TableCell>{row[0]}</TableCell>
+                                    <TableCell align="right">{row[2]}</TableCell>
+                                    <TableCell align="right">{row[3]}</TableCell>
+                                    <TableCell align="right">{row[4]}</TableCell>
+                                </TableRow>
+                            ))}
+                            <TableRow>
+                                <TableCell rowSpan={3} />
+                                <TableCell colSpan={2}>Tổng phụ</TableCell>
+                                <TableCell align="right">{invoiceSubtotal}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Thuế</TableCell>
+                                <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
+                                <TableCell align="right">{invoiceTaxes}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell colSpan={2}>Tổng</TableCell>
+                                <TableCell align="right">{invoiceTotal}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        );
+    }
 }
