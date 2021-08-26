7fa07fd88ef9dd84986ec6396fba3f6d74d4e3f2
ngocyen <buingocyen055@gmail.com>
2021-08-26 15:16:08 +0700

    fb1

--- src/components/LoginForm/Login-facebook.js
+++ src/components/LoginForm/Login-facebook.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
 import Cookies from 'universal-cookie';
-import { refreshTokenSetup } from '../utils/refreshToken';
 
 
 
@@ -10,41 +9,11 @@ const DOMAIN = process.env.REACT_APP_DOMAIN;
 
  function LoginHooksFb() {
 
-  const responseFacebook = async(res) => {
-    let data = res.profileObj;
-    const request = new Request(`${DOMAIN}/api/auth/google`, {
-      method: 'POST',
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      body: JSON.stringify(data)
-    });
+  const responseFacebook = (res) => {
+   console.log("🚀 ~ file: Login-facebook.js ~ line 16 ~ responseFacebook ~ res", res)
 
-    await fetch(request)
-        .then(res => res.json())
-        .then((result) => {
-            if (result) {
-              if(result.data.role !== 'lock'){
-                let data = {
-                  id: result.data.id,
-                  email: result.data.email,
-                  fullname: result.data.fullname,
-                  numphone: result.data.numphone
-                } 
-                cookies.set('user', data, { path: '/' });
-                window.location.reload();
-              }else if(result.data.role === 'lock'){
-                alert("Your account was lock!")
-              }
-              }
-        },
-            (error) => {
-                if (error) {
-                    console.log(error);
-                }
-            }
-        )
-    refreshTokenSetup(res);
   }
-
+  
   return (
     
     <FacebookLogin
